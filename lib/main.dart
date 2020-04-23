f4af65e9b1837e02ce8a9aae71b42359a4f4929f
Brett <bremwhit@ut.utm.edu>
2020-04-23 16:03:15 -0500

    Experimentation with routes and async code

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,13 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'QuoteCard.dart';
+import 'package:muna/pages/home.dart';
+import 'package:muna/pages/loading.dart';
+import 'package:muna/pages/choose_location.dart';
 
-void main() => runApp(MaterialApp(home: Quotelist()));
-
-class Quotelist extends StatefulWidget {
-  @override
-  _QuotelistState createState() => _QuotelistState();
-}
-
-class _QuotelistState extends State<Quotelist> {
-  /* LIST OF QUOTES HERE */
-  List<Quote> quotes = [
-    Quote(text: "quote one", author: "author one"),
-    Quote(text: "quote two", author: "author two"),
-    Quote(text: "quote three", author: "author three"),
-    Quote(text: "quote four", author: "author four"),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Quote Cards'),
-        centerTitle: true,
-        backgroundColor: Colors.greenAccent,
-      ),
-      body: Column(
-        children: quotes
-            .map((quote) => QuoteCard(
-                  quote: quote,
-                  delete: () {
-                    setState((){
-                      quotes.remove(quote);
-                    });
-                  },
-                ))
-            .toList(),
-      ),
-    );
-  }
-}
+void main() => runApp(MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
+    ));
